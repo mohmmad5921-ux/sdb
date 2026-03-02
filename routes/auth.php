69ff747a8b68da8f12c74f48705e1410796161f8
Mohammad <mohammad@MacBook-Pro-Mohammad.local>
2026-03-03 00:02:44 +0100

    security: hide login/register from public, add secret /sdb-admin login

--- routes/auth.php
+++ routes/auth.php
@@ -12,13 +12,16 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
+    // Redirect public register & login to preregister (pre-launch mode)
+    Route::get('register', fn() => redirect('/preregister'))
         ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
-
-    Route::get('login', [AuthenticatedSessionController::class, 'create'])
+    Route::get('login', fn() => redirect('/preregister'))
         ->name('login');
+
+    // Secret admin login
+    Route::get('sdb-admin', [AuthenticatedSessionController::class, 'create'])
+        ->name('admin.login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
 
